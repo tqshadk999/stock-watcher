18df4290d8c8c78b7e48a25f614ab7883e284528
tqshadk999 <tqshadk999@gmail.com>
2025-12-21 21:27:06 +0900

    Create telegram.py

---
+++ app/telegram.py
@@ -0,0 +1,7 @@
+import os, requests
+
+def send_message(text):
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    requests.post(url, data={"chat_id": chat_id, "text": text})
