443a3594b217fe56e0c87fdd7043c9da71df23f7
tqshadk999 <tqshadk999@gmail.com>
2026-01-10 15:41:39 +0900

    Update telegram.py

--- app/telegram.py
+++ app/telegram.py
@@ -1,7 +1,12 @@
-import os, requests
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_message(text):
-    token = os.getenv("TELEGRAM_TOKEN")
-    chat_id = os.getenv("CHAT_ID")
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    requests.post(url, data={"chat_id": chat_id, "text": text})
+    if not TOKEN or not CHAT_ID:
+        return
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
